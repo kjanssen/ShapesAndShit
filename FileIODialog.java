0a26f8347bec3d5514056e6cd10356d9fc6e9a4a
kgb_operative <kgb_operative@riseup.net>
2013-11-09 16:33:46 -0800

    fixed indentations

--- FileIODialog.java
+++ FileIODialog.java
@@ -5,8 +5,8 @@
 // Originally created by Dr. Watts
 // http://watts.cs.sonoma.edu
 /*
-The file contains a Dialog box class for saving a shape IO file.
-*/
+   The file contains a Dialog box class for saving a shape IO file.
+ */
 
 import java.awt.*;
 import java.awt.event.*;
@@ -15,59 +15,59 @@ import javax.swing.*;
 
 public class FileIODialog extends JDialog implements ActionListener 
 {
- 	private JPanel myPanel = null;
-        private JButton actionButton = null, cancelButton = null;
-	private JPanel buttonPanel = null;
-        private JTextField filenameText;
-	private String filename = null;
- 	private boolean answer = false;
-	public String getFilename () {return filename; }
- 	public boolean getAnswer() { return answer; }
+    private JPanel myPanel = null;
+    private JButton actionButton = null, cancelButton = null;
+    private JPanel buttonPanel = null;
+    private JTextField filenameText;
+    private String filename = null;
+    private boolean answer = false;
+    public String getFilename () {return filename; }
+    public boolean getAnswer() { return answer; }
 
-        public FileIODialog(JFrame frame, boolean modal, String action)
-	{
- 		super(frame, modal);
- 		myPanel = new JPanel();
-		getContentPane().add(myPanel);
-		myPanel.setLayout (new FlowLayout());
-		addTextAndButtons (action);
-		setTitle (action + " Shapes");
-		setLocation(300,300);
-		setSize (400,120);
- 		setVisible(true);
-	}
+    public FileIODialog(JFrame frame, boolean modal, String action)
+    {
+        super(frame, modal);
+        myPanel = new JPanel();
+        getContentPane().add(myPanel);
+        myPanel.setLayout (new FlowLayout());
+        addTextAndButtons (action);
+        setTitle (action + " Shapes");
+        setLocation(300,300);
+        setSize (400,120);
+        setVisible(true);
+    }
 
-	private void addTextAndButtons (String action)
-	{
-	        myPanel.add(new JLabel("Enter Filename (.sio extension):"));
-		filenameText = new JTextField("SavedShapes.sio", 30);
-		filenameText.addActionListener(this);
-		myPanel.add(filenameText);
-		buttonPanel = new JPanel();
-		actionButton = new JButton(action);
-		actionButton.addActionListener(this);
-		buttonPanel.add(actionButton);
-		cancelButton = new JButton("Cancel");
-		cancelButton.addActionListener(this);
-		buttonPanel.add(cancelButton); 
-		myPanel.add(buttonPanel); 
-	}
+    private void addTextAndButtons (String action)
+    {
+        myPanel.add(new JLabel("Enter Filename (.sio extension):"));
+        filenameText = new JTextField("SavedShapes.sio", 30);
+        filenameText.addActionListener(this);
+        myPanel.add(filenameText);
+        buttonPanel = new JPanel();
+        actionButton = new JButton(action);
+        actionButton.addActionListener(this);
+        buttonPanel.add(actionButton);
+        cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(this);
+        buttonPanel.add(cancelButton); 
+        myPanel.add(buttonPanel); 
+    }
 
-	public void actionPerformed(ActionEvent e) 
-	{
-		if(actionButton == e.getSource()) 
-		{
-		        filename = filenameText.getText();
-		
-			answer = true;
-			setVisible(false);
-			getContentPane().remove(myPanel);
-        	}
- 		else if(cancelButton == e.getSource()) 
-		{
- 			answer = false;
-			setVisible(false);
-		}
-       }
+    public void actionPerformed(ActionEvent e) 
+    {
+        if(actionButton == e.getSource()) 
+        {
+            filename = filenameText.getText();
+
+            answer = true;
+            setVisible(false);
+            getContentPane().remove(myPanel);
+        }
+        else if(cancelButton == e.getSource()) 
+        {
+            answer = false;
+            setVisible(false);
+        }
+    }
 } 
 
--- RightDialog.java
+++ RightDialog.java
@@ -5,9 +5,9 @@
 // Originally created by Dr. Watts
 // http://watts.cs.sonoma.edu
 /*
-Dialog box for entering color, side length, and rotation
-of a Right.
-*/
+   Dialog box for entering color, side length, and rotation
+   of a Right.
+ */
 
 import java.awt.*;
 import java.awt.event.*;
@@ -16,114 +16,114 @@ import javax.swing.*;
 
 public class RightDialog extends JDialog implements ActionListener 
 {
- 	private JPanel myPanel = null;
- 	private JButton OKButton = null, cancelButton = null;
-	private JTextField sideText;
-	private JTextField side2Text;
-	private JTextField angleText;
-	private ColorPanel colorPanel = null;
-	private JPanel buttonPanel = null;    
-	private Color currentColor = Color.white;
-	private int oldSide = 0;
-	private int side = 0;
-	private int oldSide2 = 0;
-	private int side2 = 0;
-	private double oldAngle = 0;
-	private double angle = 0;
- 	private boolean answer = false;
- 	public Color getColor() { return currentColor; }
- 	public int getSide() { return side; }
- 	public int getSide2() { return side2; }
- 	public double getAngle() { return angle; }
- 	public boolean getAnswer() { return answer; }
+    private JPanel myPanel = null;
+    private JButton OKButton = null, cancelButton = null;
+    private JTextField sideText;
+    private JTextField side2Text;
+    private JTextField angleText;
+    private ColorPanel colorPanel = null;
+    private JPanel buttonPanel = null;    
+    private Color currentColor = Color.white;
+    private int oldSide = 0;
+    private int side = 0;
+    private int oldSide2 = 0;
+    private int side2 = 0;
+    private double oldAngle = 0;
+    private double angle = 0;
+    private boolean answer = false;
+    public Color getColor() { return currentColor; }
+    public int getSide() { return side; }
+    public int getSide2() { return side2; }
+    public double getAngle() { return angle; }
+    public boolean getAnswer() { return answer; }
 
-	public RightDialog(JFrame frame, boolean modal, int x, int y, int S1, int S2, double A, Color C)
-	{
- 		super(frame, modal);
-		oldSide = S1;
-		side = oldSide;
-		oldSide2 = S2;
-		side2 = oldSide2;
-		oldAngle = A;
-		angle = oldAngle;
-		currentColor = C;
- 		myPanel = new JPanel();
-		getContentPane().add(myPanel);
-		myPanel.setLayout (new FlowLayout ());
-	 	myPanel.add(new JLabel("Select the color:"));
-		colorPanel = new ColorPanel (currentColor);
-		myPanel.add (colorPanel);
-		addTextAndButtons ();
-		setTitle ("Modify Right Dialog");
-		setLocation (x, y);
-		setSize (300,325);
- 		setVisible(true);
-	}
-	
-	private void addTextAndButtons ()
-	{
-	 	myPanel.add(new JLabel("Enter the side:"));
-		sideText = new JTextField(((Integer) side).toString(), 20);
-		sideText.addActionListener(this);
-		myPanel.add (sideText);
-	 	myPanel.add(new JLabel("Enter the other side:"));
-		side2Text = new JTextField(((Integer) side2).toString(), 20);
-		side2Text.addActionListener(this);
-		myPanel.add (side2Text);
-	 	myPanel.add(new JLabel("Enter the angle:"));
-		angleText = new JTextField(((Double) angle).toString(), 20);
-		angleText.addActionListener(this);
-		myPanel.add (angleText);
-		buttonPanel = new JPanel();
-		OKButton = new JButton("    OK    ");
-		OKButton.addActionListener(this);
-		buttonPanel.add(OKButton); 
-		cancelButton = new JButton("Cancel");
-		cancelButton.addActionListener(this);
-		buttonPanel.add(cancelButton); 
-		myPanel.add(buttonPanel); 
-	}
+    public RightDialog(JFrame frame, boolean modal, int x, int y, int S1, int S2, double A, Color C)
+    {
+        super(frame, modal);
+        oldSide = S1;
+        side = oldSide;
+        oldSide2 = S2;
+        side2 = oldSide2;
+        oldAngle = A;
+        angle = oldAngle;
+        currentColor = C;
+        myPanel = new JPanel();
+        getContentPane().add(myPanel);
+        myPanel.setLayout (new FlowLayout ());
+        myPanel.add(new JLabel("Select the color:"));
+        colorPanel = new ColorPanel (currentColor);
+        myPanel.add (colorPanel);
+        addTextAndButtons ();
+        setTitle ("Modify Right Dialog");
+        setLocation (x, y);
+        setSize (300,325);
+        setVisible(true);
+    }
 
-	public void actionPerformed(ActionEvent e) 
-	{
-		if(OKButton == e.getSource()) 
-		{
-			currentColor = colorPanel.getColor();
-			answer = true;
-			setVisible(false);
-			getContentPane().remove(myPanel);
-			try
-			{
-				side = Integer.parseInt (sideText.getText());
-			}
-			catch (NumberFormatException ex)
-			{
-				side = oldSide;
-			}
-			try
-			{
-				side2 = Integer.parseInt (side2Text.getText());
-			}
-			catch (NumberFormatException ex)
-			{
-				side2 = oldSide2;
-			}
-			try
-			{
-				angle = Double.parseDouble (angleText.getText());
-			}
-			catch (NumberFormatException ex)
-			{
-				angle = oldAngle;
-			}
-			answer = side == 0 || side2 == 0 ? false : true;
-        	}
- 		else if(cancelButton == e.getSource()) 
-		{
- 			answer = false;
-			setVisible(false);
-		}
-       }
- 
+    private void addTextAndButtons ()
+    {
+        myPanel.add(new JLabel("Enter the side:"));
+        sideText = new JTextField(((Integer) side).toString(), 20);
+        sideText.addActionListener(this);
+        myPanel.add (sideText);
+        myPanel.add(new JLabel("Enter the other side:"));
+        side2Text = new JTextField(((Integer) side2).toString(), 20);
+        side2Text.addActionListener(this);
+        myPanel.add (side2Text);
+        myPanel.add(new JLabel("Enter the angle:"));
+        angleText = new JTextField(((Double) angle).toString(), 20);
+        angleText.addActionListener(this);
+        myPanel.add (angleText);
+        buttonPanel = new JPanel();
+        OKButton = new JButton("    OK    ");
+        OKButton.addActionListener(this);
+        buttonPanel.add(OKButton); 
+        cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(this);
+        buttonPanel.add(cancelButton); 
+        myPanel.add(buttonPanel); 
+    }
+
+    public void actionPerformed(ActionEvent e) 
+    {
+        if(OKButton == e.getSource()) 
+        {
+            currentColor = colorPanel.getColor();
+            answer = true;
+            setVisible(false);
+            getContentPane().remove(myPanel);
+            try
+            {
+                side = Integer.parseInt (sideText.getText());
+            }
+            catch (NumberFormatException ex)
+            {
+                side = oldSide;
+            }
+            try
+            {
+                side2 = Integer.parseInt (side2Text.getText());
+            }
+            catch (NumberFormatException ex)
+            {
+                side2 = oldSide2;
+            }
+            try
+            {
+                angle = Double.parseDouble (angleText.getText());
+            }
+            catch (NumberFormatException ex)
+            {
+                angle = oldAngle;
+            }
+            answer = side == 0 || side2 == 0 ? false : true;
+        }
+        else if(cancelButton == e.getSource()) 
+        {
+            answer = false;
+            setVisible(false);
+        }
+    }
+
 } 
 
